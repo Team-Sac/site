768043b8ad836de3ac68a3f5c3155d119eeeaaab
cannonbark <geoffrey.parrier@gmail.com>
2023-02-19 00:03:35 +0100

    Adding directus support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  ssr: true,
+  modules: [
+    'nuxt-windicss',
+    'nuxt-directus',
+  ],
+  directus: {
+    url: 'https://directus.teamsac.xyz/'
+  }
 })
