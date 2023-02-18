3fdd14f7a39d84615fbe8eb99752df2426123c42
cannonbark <geoffrey.parrier@gmail.com>
2023-02-19 00:12:44 +0100

    Adding layouts

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <slot></slot>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Dingue une première page</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index"
+}
+</script>
+
+<style scoped>
+
+</style>
