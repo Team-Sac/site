16b9e2446f956fe6e64af85565389f80954ac6bb
cannonbark <geoffrey.parrier@gmail.com>
2023-03-12 02:02:39 +0100

    site-10 Login to Directus

--- app.vue
+++ app.vue
@@ -4,7 +4,7 @@
   </NuxtLayout>
 </template>
 
-<script setup lang="ts">
+<script lang="ts" setup>
 import {useRoute} from "vue-router";
 
 const route = useRoute();
@@ -13,5 +13,23 @@ useHead({
   titleTemplate: () => {
     return route.path !== "/" ? `${ route.path.split("/")[1] } - Team Sac` : 'Team Sac';
   },
+})
+
+
+onMounted(async () => {
+  const user = useDirectusUser();
+  if (user.value) return;
+
+  const {setToken} = useDirectusAuth();
+  try {
+    const {data: {access_token}} = await (await fetch(`${useDirectusUrl()}auth/refresh/`, {
+      method: 'POST',
+      credentials: 'include'
+    })).json();
+
+    setToken(access_token);
+  } catch (e) {
+    console.log(e);
+  }
 })
 </script>
