bc6afbac01b1efa3cf4912ad40e88367b588e9f0
cannonbark <geoffrey.parrier@gmail.com>
2023-03-22 16:58:15 +0100

    fix: Setting Storybook to Typescript & updating Package.json

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,6 @@
+import {ViteConfig} from '@nuxt/schema';
+import {ViteOptions} from '@nuxt/vite-builder';
+
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
@@ -6,12 +9,19 @@ const config = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
   ],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
   framework: {
     name: '@storybook/vue3-vite',
     options: {},
   },
   docs: {
     autodocs: 'tag',
+  },
+  async viteFinal(config: ViteConfig, options: ViteOptions) {
+    // Add your configuration here
+    return config;
   },
 };
 export default config;
