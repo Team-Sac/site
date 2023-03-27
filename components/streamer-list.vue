fd2cfcededd585e56d7e321704f7bab01bf8bac4
YoanRouleau <yoan-rouleau@live.fr>
2023-03-27 15:53:51 +0200

    Resolving conflicts between master and feature

--- components/streamer-list.vue
+++ components/streamer-list.vue
@@ -1,7 +1,25 @@
+<script lang="ts" setup>
+const { getItems } = useDirectusItems();
+const router = useRouter();
+
+interface Streamer {
+  id?: string | number;
+  sac: boolean;
+  online: boolean;
+}
+
+const fetchStreamers = async () => {
+  try {
+    // var filters = { content: "testcontent", title: "Test1" };
+    return await getItems<Streamer>({
+      collection: 'streamers',
+    });
+  } catch (e) {}
+};
+
+const streamers = await fetchStreamers();
+</script>
 <template>
   <p>Liste Streamers</p>
 </template>
-
-<script lang="ts" setup></script>
-
 <style scoped></style>
