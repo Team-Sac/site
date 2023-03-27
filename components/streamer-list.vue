4b260a491ebb73c3508cbee4d18068e6c685bd8b
YoanRouleau <yoan-rouleau@live.fr>
2023-03-27 18:17:30 +0200

    Retrieving Streamers data in component

--- components/streamer-list.vue
+++ components/streamer-list.vue
@@ -1,31 +1,30 @@
 <script lang="ts" setup>
-
 const { getItems } = useDirectusItems();
 const router = useRouter();
 
 interface Streamer {
-    id?: string | number;
-    sac: boolean;
-    online: boolean;
+  id: string;
+  sac: boolean;
+  online: boolean;
 }
 
+const token = useDirectusToken();
+const user = useDirectusUser();
+
 const fetchStreamers = async () => {
-    try {
-        // var filters = { content: "testcontent", title: "Test1" };
-        return await getItems<Streamer>({
-            collection: "streamers",
-        });
-    } catch (e) {}
+  try {
+    return await getItems<Streamer>({
+      collection: 'streamers',
+    });
+  } catch (e) {}
 };
 
 const streamers = await fetchStreamers();
-
 </script>
-
-
 <template>
   <p>Liste Streamers</p>
+  <div v-for="streamer in streamers">
+    {{ streamer.id }} - {{ streamer.online ? 'En ligne' : 'Hors ligne' }}
+  </div>
 </template>
-
-
 <style scoped></style>
