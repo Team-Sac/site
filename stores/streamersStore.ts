d0c534f9471e79f5e0c0485b4d0a01f8f56ceedd
cannonbark <geoffrey.parrier@gmail.com>
2023-04-03 22:42:18 +0200

    feat: Rm getStreamers API & adding getting user token to streamersStore

--- stores/streamersStore.ts
+++ stores/streamersStore.ts
@@ -1,34 +1,17 @@
 import { defineStore } from 'pinia';
+// eslint-disable-next-line import/extensions
+import { useUserStore } from '@/stores/userStore';
 
 // eslint-disable-next-line import/prefer-default-export
 export const useStreamersStore = defineStore('streamers', () => {
   const bearer = ref({});
 
   async function getStreamers() {
-    /* await useAsyncData('getBearer', () => $fetch('/api/getBearerToken')).then(
-      (res) => {
-        bearer.value = res.data.value.access_token;
-      }
-    );
-    console.log(bearer.value);
+    const { getToken } = useUserStore();
 
-    const streamers = await fetch('http://localhost:3000/api/getStreamers', {
-      method: 'POST',
-      body: {
-        bearerToken: bearer.value,
-      },
-    });
-    console.log(streamers);
-    return streamers;
-  } */
-    // TODO: Move it to a userStore
-    // const test = console.log(test.data.value);
-    /* return useFetch('/api/getStreamers', {
-      method: 'POST',
-      body: {
-        bearerToken: bearer.value,
-      },
-    }); */
+    const token = await getToken();
+
+    console.log(token.value);
   }
 
   return { bearer, getStreamers };
