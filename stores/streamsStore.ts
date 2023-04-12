acd100e3607a8cf2effb0e55c131163ad66d8111
cannonbark <geoffrey.parrier@gmail.com>
2023-04-12 17:17:17 +0200

    feat: Add and remove streams functions and place elements in a grid

--- stores/streamsStore.ts
+++ stores/streamsStore.ts
@@ -2,7 +2,15 @@ import { defineStore } from 'pinia';
 
 // eslint-disable-next-line import/prefer-default-export
 export const useStreamsStore = defineStore('streams', () => {
-  const streams = ref<string[]>(['deuxlames', 'jeykon_']);
+  const streams = ref<string[]>([]);
 
-  return { streams };
+  const add = (streamer: string) => {
+    streams.value.push(streamer);
+  };
+
+  const remove = (streamer: string) => {
+    streams.value.splice(streams.value.indexOf(streamer), 1);
+  };
+
+  return { streams, add, remove };
 });
