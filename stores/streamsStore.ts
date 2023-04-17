76469cf0d45d47f22f11cd73234e0ccc6ae9db28
cannonbark <geoffrey.parrier@gmail.com>
2023-04-18 00:15:00 +0200

    feat: Adding toggle to add or remove dynamically an element from streams store

--- stores/streamsStore.ts
+++ stores/streamsStore.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia';
 
 // eslint-disable-next-line import/prefer-default-export
 export const useStreamsStore = defineStore('streams', () => {
-  const streams = ref<string[]>([]);
+  const streams = ref<string[]>(['Cannonbark', 'DeuxLames']);
 
   const add = (streamer: string) => {
     streams.value.push(streamer);
@@ -12,5 +12,18 @@ export const useStreamsStore = defineStore('streams', () => {
     streams.value.splice(streams.value.indexOf(streamer), 1);
   };
 
-  return { streams, add, remove };
+  const toggle = (streamer: string) => {
+    if (streams.value.includes(streamer)) {
+      remove(streamer);
+    } else {
+      add(streamer);
+    }
+  };
+
+  return {
+    streams,
+    add,
+    remove,
+    toggle,
+  };
 });
