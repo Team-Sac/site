21e3ca239c17fa2b501d738794d25091ff60913a
YoanRouleau <yoan-rouleau@live.fr>
2023-04-19 17:20:47 +0200

    Linking directus online status to twitch api data

--- components/streamer-list.vue
+++ components/streamer-list.vue
@@ -8,6 +8,7 @@ const streamers = ref();
 onMounted(async () => {
   const res = await streamersStore.getStreamers();
   streamers.value = streamersStore.streamers;
+  console.log(streamers.value);
 });
 </script>
 
@@ -19,12 +20,14 @@ onMounted(async () => {
         v-for="streamer in streamers"
         :key="streamer"
         class="streamerlist-element"
+        :class="{ isOnline: !streamer.online }"
       >
         <img
           :src="streamer.profile_image_url"
-          :alt="`image de profil ${profile_image_url}`"
+          :alt="`image de profil ${streamer.display_name}`"
           class="streamerlist-icon"
         />
+        <span v-if="streamer.online" class="streamerlist-online">LIVE</span>
       </div>
     </div>
   </div>
@@ -34,9 +37,18 @@ onMounted(async () => {
   @apply h-screen flex items-center;
 }
 .streamerlist-container {
-  @apply max-h-[70%] overflow-auto flex flex-col space-y-7;
+  @apply h-[70%] overflow-auto flex flex-col gap-y-6;
 }
 .streamerlist-icon {
   @apply rounded-full w-20 h-20;
+}
+.streamerlist-element {
+  @apply relative;
+}
+.streamerlist-element.isOnline .streamerlist-icon {
+  @apply border-4 border-white;
+}
+.streamerlist-online {
+  @apply absolute -bottom-2.5 left-1/2 -translate-x-1/2;
 }
 </style>
