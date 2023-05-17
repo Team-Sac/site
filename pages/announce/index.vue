b325fdc429b21338820037de1a1837f70b02736f
cannonbark <geoffrey.parrier@gmail.com>
2023-05-17 02:26:54 +0200

    feat: Creating a store to communicate properly with the API and adding everything to the front

--- pages/announce/index.vue
+++ pages/announce/index.vue
@@ -1,5 +1,29 @@
-<script lang="ts" setup></script>
+<template>
+  <div>
+    <ClientOnly fallback="Chargement des annonces..." fallback-tag="span">
+      <article
+        v-for="{ id, title, content, date_created } in announces"
+        :key="id"
+        class="announce"
+      >
+        <h2>{{ title }}</h2>
+        <p>{{ content }}</p>
+        <p>
+          <time :datetime="date_created">{{
+            new Date(date_created).toLocaleDateString('FR-fr')
+          }}</time>
+        </p>
+      </article>
+    </ClientOnly>
+  </div>
+</template>
 
-<template>Annonce</template>
+<script lang="ts" setup>
+import { Announce, useAnnouncesStore } from '~/stores/announcesStore';
+
+const { announces, fetchAnnounces }: { announces: Announce[] } =
+  useAnnouncesStore();
+await fetchAnnounces();
+</script>
 
 <style scoped></style>
