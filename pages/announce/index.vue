138a7fe23415e57eb080a2694135e93fb5ad7442
cannonbark <geoffrey.parrier@gmail.com>
2023-05-18 22:21:37 +0200

    feat: Adding creating and deleting Announce in case of role 'Sac' or 'Admin'

--- pages/announce/index.vue
+++ pages/announce/index.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
+    <form v-if="hasRight">
+      <label for="title">
+        Title
+        <input id="title" v-model="titleNew" name="title" type="text" />
+      </label>
+      <label for="content">
+        Content
+        <textarea
+          id="content"
+          v-model="contentNew"
+          cols="30"
+          name="content"
+          rows="10"
+        ></textarea>
+      </label>
+      <button @click="newAnnounce">Créer une annonce</button>
+    </form>
     <ClientOnly fallback="Chargement des annonces..." fallback-tag="span">
       <article
         v-for="{ id, title, content, date_created } in announces"
@@ -10,9 +27,15 @@
         <p>{{ content }}</p>
         <p>
           <time :datetime="date_created">{{
-            new Date(date_created).toLocaleDateString('FR-fr')
+            new Date(date_created).toLocaleString('FR-fr', {
+              dateStyle: 'full',
+              timeStyle: 'short',
+            })
           }}</time>
         </p>
+        <button v-if="hasRight" @click="() => removeAnnounce(id)">
+          <i class="fas fa-trash"></i>
+        </button>
       </article>
     </ClientOnly>
   </div>
@@ -20,10 +43,34 @@
 
 <script lang="ts" setup>
 import { Announce, useAnnouncesStore } from '~/stores/announcesStore';
+import { DirectusUser } from 'nuxt-directus/dist/runtime/types';
 
-const { announces, fetchAnnounces }: { announces: Announce[] } =
-  useAnnouncesStore();
+const user = computed<DirectusUser>(() => useDirectusUser().value);
+
+const hasRight = computed(() => {
+  if (!user.value) return false;
+  return [
+    'e5c8b057-49ff-4781-af76-ab555f5a0465',
+    'bdb50fa6-c41d-4b5f-8d23-91d4f3748533',
+  ].includes(user.value?.role);
+});
+
+const { fetchAnnounces, createAnnounce, deleteAnnounce } = useAnnouncesStore();
+
+const announces = computed<Announce[]>(() => useAnnouncesStore().announces);
+
 await fetchAnnounces();
+
+const titleNew = ref('');
+const contentNew = ref('');
+
+const newAnnounce = async (e) => {
+  e.preventDefault();
+  await createAnnounce({ title: titleNew.value, content: contentNew.value });
+};
+const removeAnnounce = async (id) => {
+  await deleteAnnounce(id);
+};
 </script>
 
 <style scoped></style>
