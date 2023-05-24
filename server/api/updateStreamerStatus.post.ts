10a466f9e4b85c66c4d05ae25018aca944ba30ed
cannonbark <geoffrey.parrier@gmail.com>
2023-05-24 18:49:33 +0200

    fix: Remove filter for only Sac to show up

--- server/api/updateStreamerStatus.post.ts
+++ server/api/updateStreamerStatus.post.ts
@@ -1,15 +1,19 @@
 import crypto from 'crypto';
+import { setResponseStatus } from 'h3';
 
-export default defineEventHandler(async (event) => {
+// eslint-disable-next-line consistent-return
+export default defineEventHandler(async (event): Promise<void> => {
   const body = await readRawBody(event);
   const { headers } = event.node.req;
 
   // Notification request headers
   const TWITCH_MESSAGE_ID = 'Twitch-Eventsub-Message-Id'.toLowerCase();
   // eslint-disable-next-line
-  const TWITCH_MESSAGE_TIMESTAMP = 'Twitch-Eventsub-Message-Timestamp'.toLowerCase();
+  const TWITCH_MESSAGE_TIMESTAMP =
+    'Twitch-Eventsub-Message-Timestamp'.toLowerCase();
   // eslint-disable-next-line
-  const TWITCH_MESSAGE_SIGNATURE = 'Twitch-Eventsub-Message-Signature'.toLowerCase();
+  const TWITCH_MESSAGE_SIGNATURE =
+    'Twitch-Eventsub-Message-Signature'.toLowerCase();
 
   const MESSAGE_TYPE = 'Twitch-Eventsub-Message-Type'.toLowerCase();
 
@@ -52,63 +56,36 @@ export default defineEventHandler(async (event) => {
   const hmac = HMAC_PREFIX + getHmac(secret, message);
 
   // Signatures does matches
-  if (verifyMessage(hmac, headers[TWITCH_MESSAGE_SIGNATURE]) === true) {
+  if (verifyMessage(hmac, headers[TWITCH_MESSAGE_SIGNATURE])) {
     // Get JSON object from body, so you can process the message.
-    const notification = JSON.parse(body);
+    const notification = body ? JSON.parse(body) : {};
 
     if (MESSAGE_TYPE_NOTIFICATION === headers[MESSAGE_TYPE]) {
-      // TODO: Do something with the event's data.
-      console.log(`Event type: ${notification.subscription.type}`);
-      console.log(JSON.stringify(notification.event, null, 4));
+      try {
+        await fetch(
+          `https://directus.teamsac.xyz/items/streams/${notification.event.broadcaster_user_name}`,
+          {
+            method: 'PATCH',
+            headers: {
+              'Content-Type': 'application/json',
+              authorization: `Bearer ${process.env.NUXT_DIRECTUS_SECRET_KEY}`,
+            },
+            body: JSON.stringify({
+              online: true,
+            }),
+          },
+        );
+      } catch (e) {}
 
-      fetch(
-        'https://directus.teamsac.xyz/flows/trigger/7d41730f-4083-4bd1-b6e0-ca8979356368',
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            id: notification.event.broadcaster_user_name,
-            status: notification.subscription.type === 'stream.online',
-          }),
-        },
-      )
-        .then((response) => response.json())
-        .then((jsonResponse) => {
-          console.log(jsonResponse);
-        });
-
-      switch (notification.subscription.type) {
-        case 'stream.online':
-          console.log('Going live');
-          break;
-        case 'stream.offline':
-          console.log('Stopping live');
-          break;
-        default:
-          console.log(`Live status updated to ${notification.event.type}`);
-          break;
-      }
       setResponseStatus(event, 204);
     } else if (MESSAGE_TYPE_VERIFICATION === headers[MESSAGE_TYPE]) {
-      console.log('Subscription registered.');
       return notification.challenge;
     } else if (MESSAGE_TYPE_REVOCATION === headers[MESSAGE_TYPE]) {
       setResponseStatus(event, 204);
-      console.log(`${notification.subscription.type} notifications revoked!`);
-      console.log(`reason: ${notification.subscription.status}`);
-      console.log(
-        `condition: ${JSON.stringify(
-          notification.subscription.condition,
-          null,
-          4,
-        )}`,
-      );
     } else {
       setResponseStatus(event, 204);
-      console.log(`Unknown message type: ${headers[MESSAGE_TYPE]}`);
     }
   } else {
-    console.log("403 Signatures didn't match."); // Signatures didn't match.
     setResponseStatus(event, 403);
   }
 });
