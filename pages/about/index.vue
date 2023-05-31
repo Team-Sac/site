7bf774edf6fb7c1996a8cef8991e708e528049e7
cannonbark <geoffrey.parrier@gmail.com>
2023-05-31 15:03:34 +0200

    feat: Adding /about

---
+++ pages/about/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <div v-for="sac in sacs" :key="sac.id">
+      <h2>{{ sac.id }}</h2>
+      <p>{{ sac.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Streamer, useStreamersStore } from '~/stores/streamersStore';
+
+const streamersStore = useStreamersStore();
+
+await streamersStore.fetchDirectusStreamers();
+const sacs = await computed<Streamer[]>(() => streamersStore.sacs);
+</script>
+
+<style scoped></style>
