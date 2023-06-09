566f5927681ef417c1171b8da038f41e7ed966a4
cannonbark <geoffrey.parrier@gmail.com>
2023-06-09 14:16:51 +0200

    feat: Adding Unlighthouse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
       link: [
         {
           rel: 'stylesheet',
-          href: 'https://use.fontawesome.com/releases/v5.15.4/css/all.css',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css',
         },
         {
           rel: 'icon',
