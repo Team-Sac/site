254f9f1b0c30117200acb82f30348c7e40c960d3
cannonbark <geoffrey.parrier@gmail.com>
2023-06-23 21:31:29 +0200

    feat: Setting style of about page

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,8 +1,33 @@
 <template>
   <div class="about">
-    <div v-for="sac in sacs" :key="sac.id">
-      <h2>{{ sac.id }}</h2>
-      <p>{{ sac.description }}</p>
+    <div v-for="sac in sacs" :key="sac.id" class="card">
+      <div class="image-wrapper">
+        <img
+          :alt="`image de profil de ${sac.id}`"
+          :src="sac.profile_image_url"
+          height="300"
+          width="300"
+        />
+        <div class="image-filter"></div>
+      </div>
+      <div class="title">
+        <div class="name">
+          <h2>{{ sac.id }}</h2>
+        </div>
+        <div class="more">
+          <NuxtLink
+            :to="`https://www.twitch.tv/${sac.id}`"
+            class="twitch"
+            target="_blank"
+          >
+            <i class="fab fa-twitch"></i>
+          </NuxtLink>
+        </div>
+      </div>
+      <div class="description">
+        <div></div>
+        <p>{{ sac.description }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -16,4 +41,85 @@ await streamersStore.fetchDirectusStreamers();
 const sacs = await computed<Streamer[]>(() => streamersStore.sacs);
 </script>
 
-<style scoped></style>
+<style scoped>
+.about {
+  @apply w-full 2xl:h-[calc(100vh-10.7rem)] grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-[7.5%] justify-items-center px-40 pb-16;
+}
+
+.card {
+  @apply relative max-w-full px-[31px] py-[27px] aspect-square rounded-lg overflow-hidden content-center flex flex-col justify-between;
+}
+
+.image-wrapper {
+  @apply absolute h-full min-w-full aspect-square top-0 left-0;
+}
+
+.image-wrapper > img {
+  @apply relative object-cover bg-cover w-full;
+}
+
+.image-wrapper > div {
+  @apply absolute top-0 left-0 w-[calc(100%+1px)] aspect-square bg-gradient-to-bl from-[hsla(231,74%,9%,0.74)] to-[hsla(230,62%,14%,0.71)] opacity-75;
+}
+
+.title,
+.description {
+  @apply relative;
+}
+
+.title {
+  @apply flex justify-between text-[26px];
+}
+
+.description > div {
+  @apply absolute top-0 left-0 w-[calc(100%+1px)] h-full bg-gradient-to-bl from-[hsl(231,73%,9%)] to-[hsla(230,84%,10%,0.5)] backdrop-blur-[60px] opacity-70 rounded-lg;
+}
+
+.description > p {
+  @apply relative text-[14px] px-3 py-2;
+}
+
+/*.about {
+  @apply h-[calc(100%-10.7rem)] w-full grid grid-cols-3 grid-rows-2 gap-[7.5%] justify-items-center px-40 pb-16;
+}
+
+.card {
+  @apply relative h-full max-w-full px-[31px] py-[27px] aspect-square rounded-lg overflow-hidden content-center flex flex-col justify-between;
+}
+
+.card > img {
+  @apply relative pointer-events-none top-0 left-0 aspect-square max-h-full;
+}
+
+.image-wrapper {
+  @apply !absolute w-full aspect-square max-h-full top-0 left-0 object-cover;
+}
+
+.image-filter {
+  @apply relative w-full top-0 left-0;
+}
+
+.card > div {
+  @apply relative;
+}
+
+.title {
+  @apply flex gap-[72px] justify-between;
+}
+
+.name {
+  @apply font-heading text-[26px];
+}
+
+.more {
+  @apply bg-white py-[10px] rounded-lg text-gray-900 text-center gap-[10px];
+}
+
+.more > .twitch {
+  @apply p-[10px];
+}
+
+.description {
+  @apply flex flex-col gap-[10px] rounded-xl bg-gradient-to-bl from-[hsla(231,73%,9%,0.6)] to-[hsla(230,84%,10%,0.1)] backdrop-blur-[60px];
+}*/
+</style>
