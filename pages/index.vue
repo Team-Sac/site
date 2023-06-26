8bd060e6f8c5bf154f80eb07aefb3d33f4bd2d9e
cannonbark <geoffrey.parrier@gmail.com>
2023-06-26 19:06:19 +0200

    feat: Adapting / to mobile size and block certain fonctionnalities

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,31 @@
 <template>
   <div id="twitch-streams" :class="gridSize">
-    <StreamerList @change-size="changeSize"></StreamerList>
-    <LazyTwitchStreamsMultiTwitchStreams></LazyTwitchStreamsMultiTwitchStreams>
+    <StreamerList v-if="width >= 1024" @change-size="changeSize"></StreamerList>
+    <LazyTwitchStreamsMultiTwitchStreams
+      v-if="width >= 1024"
+    ></LazyTwitchStreamsMultiTwitchStreams>
     <LazyTwitchStreamsMultiTwitchChats
+      v-if="width >= 1024"
       @change-size="changeSize"
     ></LazyTwitchStreamsMultiTwitchChats>
+
+    <div v-if="width < 1024" class="screen-too-small">
+      <div>
+        <p>
+          Vous êtes sur un écran trop petit pour être pris en charge par le
+          site.
+        </p>
+        <p>
+          Pour accéder à toutes les fonctionnalités, passez sur un ordinateur.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { useWindowSize } from '@vueuse/core';
+
 const listSize = ref<'minmax(0,2fr)' | '30px'>('minmax(0,2fr)');
 const chatSize = ref<'minmax(0,3fr)' | '60px' | '30px'>('minmax(0,3fr)');
 const gridSize = ref<
@@ -16,6 +33,7 @@ const gridSize = ref<
   | 'grid-cols-[30px,7fr,minmax(0,3fr)]'
   | 'grid-cols-[minmax(0,2fr),7fr,60px]'
   | 'grid-cols-[minmax(0,2fr),7fr,30px]'
+  | 'grid-cols-[30px,7fr,60px]'
   | 'grid-cols-[30px,7fr,30px]'
 >('grid-cols-[minmax(0,2fr),7fr,minmax(0,3fr)]');
 
@@ -49,10 +67,19 @@ const changeSize = ({
   }
   gridSize.value = `grid-cols-[${listSize.value},7fr,${chatSize.value}]`;
 };
+
+const { width } = useWindowSize();
 </script>
 
 <style scoped>
 #twitch-streams {
-  @apply w-full h-[calc(100vh-9rem)] grid overflow-hidden px-40 pb-16;
+  @apply w-full h-[calc(100vh-9rem)] lg:grid lg:overflow-hidden lg:px-40 lg:pb-16;
+}
+
+.screen-too-small {
+  @apply h-full w-full flex flex-col justify-center items-center;
+}
+.screen-too-small > div {
+  @apply p-6 rounded-3xl bg-gray-900;
 }
 </style>
