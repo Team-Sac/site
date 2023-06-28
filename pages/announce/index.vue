411e493264097684172e6f34b3e0269364fea740
cannonbark <geoffrey.parrier@gmail.com>
2023-06-29 01:41:24 +0200

    feat: Adding modal popup and making it work

--- pages/announce/index.vue
+++ pages/announce/index.vue
@@ -1,90 +1,40 @@
 <template>
-  <div>
-    <form v-if="hasRight">
-      <label for="title">
-        Title
-        <input id="title" v-model="titleNew" name="title" type="text" />
-      </label>
-      <label for="content">
-        Content
-        <textarea
-          id="content"
-          v-model="contentNew"
-          cols="30"
-          name="content"
-          rows="10"
-        ></textarea>
-        <label for="url">
-          URL to an announce or an image
-          <input id="url" v-model="urlNew" name="url" type="text" />
-        </label>
-      </label>
-      <button @click="newAnnounce">Créer une annonce</button>
-    </form>
+  <div id="about-page">
+    <announce-edit
+      :announce-id="announceId"
+      :dialog-event="dialogEvent"
+    ></announce-edit>
     <ClientOnly fallback="Chargement des annonces..." fallback-tag="span">
-      <article
-        v-for="{ id, title, content, url, date_created } in announces"
-        :key="id"
-        class="announce"
-      >
-        <h2>{{ title }}</h2>
-        <p>{{ content }}</p>
-        <p>
-          <time :datetime="date_created">{{
-            new Date(date_created).toLocaleString('FR-fr', {
-              dateStyle: 'full',
-              timeStyle: 'short',
-            })
-          }}</time>
-        </p>
-        <button v-if="hasRight" @click="() => removeAnnounce(id)">
-          <i class="fas fa-trash"></i>
-        </button>
-        <NuxtLink v-if="url" :to="url" target="_blank"
-          >Plus de Détails</NuxtLink
-        >
-      </article>
+      <announce-element
+        v-for="announce in announces"
+        :key="announce.id"
+        :announce-id="announce.id"
+        @open-announce-editor="editAnnounce"
+      ></announce-element>
     </ClientOnly>
   </div>
 </template>
 
 <script lang="ts" setup>
 import { Announce, useAnnouncesStore } from '~/stores/announcesStore';
-import { DirectusUser } from 'nuxt-directus/dist/runtime/types';
-
-const user = computed<DirectusUser>(() => useDirectusUser().value);
-
-const hasRight = computed(() =>
-  [
-    'e5c8b057-49ff-4781-af76-ab555f5a0465',
-    'bdb50fa6-c41d-4b5f-8d23-91d4f3748533',
-  ].includes(user.value?.role),
-);
-
-const { fetchAnnounces, createAnnounce, deleteAnnounce } = useAnnouncesStore();
 
 const announces = computed<Announce[]>(() => useAnnouncesStore().announces);
 
+const { fetchAnnounces } = useAnnouncesStore();
+
 await fetchAnnounces();
 
-const titleNew = ref('');
-const contentNew = ref('');
-const urlNew = ref('');
+const announceId = ref<string>();
+const dialogEvent = ref<boolean>(false);
 
-const newAnnounce = async (e) => {
-  e.preventDefault();
-  await createAnnounce({
-    title: titleNew.value,
-    content: contentNew.value,
-    url: urlNew.value,
-  });
-  titleNew.value = '';
-  contentNew.value = '';
-  urlNew.value = '';
-};
-const removeAnnounce = async (id) => {
-  await deleteAnnounce(id);
+const editAnnounce = (idAnnounce: number) => {
+  announceId.value = idAnnounce.toString();
+  dialogEvent.value = !dialogEvent.value;
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+#about-page {
+  @apply min-h-[calc(100vh-9rem)];
+}
+</style>
