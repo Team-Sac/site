5814130afd636f0a3aa8816ba597776e4c38590f
cannonbark <geoffrey.parrier@gmail.com>
2023-06-29 15:26:58 +0200

    feat: Adding Style for announce page

--- components/announce/announce-element.vue
+++ components/announce/announce-element.vue
@@ -1,24 +1,35 @@
 <template>
   <article class="announce">
-    <h2>{{ announce.title }}</h2>
-    <p>{{ announce.content }}</p>
-    <p>
-      <time :datetime="announce.date_created">{{
-        new Date(announce.date_created).toLocaleString('FR-fr', {
-          dateStyle: 'full',
-          timeStyle: 'short',
-        })
-      }}</time>
-    </p>
-    <button v-if="hasRight" @click="editAnnounce">
-      <i class="fas fa-pen"></i>
-    </button>
-    <button v-if="hasRight" @click="() => removeAnnounce">
-      <i class="fas fa-trash"></i>
-    </button>
-    <NuxtLink v-if="announce.url" :to="announce.url" target="_blank"
-      >Plus de Détails</NuxtLink
-    >
+    <div class="background-announce"></div>
+    <div class="content">
+      <h2>{{ announce.title }}</h2>
+      <div>
+        <p>{{ announce.content }}</p>
+        <NuxtLink
+          v-if="announce.url"
+          :title="announce.url"
+          :to="announce.url"
+          class="more"
+          target="_blank"
+        >
+          <i class="fas fa-link fa-lg"></i>
+        </NuxtLink>
+      </div>
+    </div>
+    <time :datetime="announce.date_created">{{
+      new Date(announce.date_created).toLocaleString('FR-fr', {
+        dateStyle: 'full',
+        timeStyle: 'short',
+      })
+    }}</time>
+    <div v-if="hasRight" class="announce-actions">
+      <button title="Éditer" @click="editAnnounce">
+        <i class="fas fa-pen fa-lg"></i>
+      </button>
+      <button title="Supprimer" @click="() => removeAnnounce">
+        <i class="fas fa-trash fa-lg"></i>
+      </button>
+    </div>
   </article>
 </template>
 
@@ -57,4 +68,44 @@ const removeAnnounce = async () => {
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.announce {
+  @apply relative p-5 rounded-3xl shadow-md shadow-[rgba(0,0,0,.25)] overflow-hidden;
+}
+
+.content {
+  @apply relative flex flex-col gap-[1.5625rem];
+}
+
+.content > h2 {
+  @apply text-[1.25rem] leading-[1.6875rem] font-medium tracking-[0.02813rem] text-[#A0AEC0];
+}
+
+.content > div {
+  @apply grid grid-cols-[1fr,40px];
+}
+
+.more {
+  @apply w-full flex justify-center items-center;
+}
+
+i {
+  @apply w-full hover:bg-gray-800 px-2 py-3;
+}
+
+.announce > time {
+  @apply absolute top-0 right-0 p-5;
+}
+
+.announce-actions {
+  @apply relative flex justify-end gap-1 w-full;
+}
+
+.announce-actions > button {
+  @apply w-[40px];
+}
+
+.background-announce {
+  @apply absolute top-0 left-0 w-full h-full bg-white opacity-5;
+}
+</style>
